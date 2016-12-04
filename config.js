ac65be775a1decf9fe99c40e5910e1fdd2876ea4
Aruna Herath <arunabherath@gmail.com>
2016-12-04 19:53:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  authyApiKey: "m3XepId5cuWbiRO3cJEwh12rFS6GZQIc",
+  authyApiKey: "",
   mongoUrl: process.env.MONGOLAB_URI || process.env.MONGO_URL,
 };
